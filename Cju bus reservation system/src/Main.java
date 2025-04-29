42d0ba29ae5f1062cbbf300171a8c624bdc8658a
minjee <jimin79546@gmail.com>
2025-04-29 17:36:35 +0900

    2025-04-29 1

---
+++ Cju bus reservation system/src/Main.java
@@ -0,0 +1,27 @@
+package  reservation;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Reserve seat = new Reserve();
+        seat.run();
+    }
+}
+class Reserve {
+    public void run() {
+         static int Total_seat = 20;
+         static Scanner scanner = new Scanner(System.in);
+         static Random random = new Random();
+
+
+
+
+
+
+
+
+    }
+}
