7c9eedd6214ca6b056fa2f105395de9418e53508
minjee <jimin79546@cju.ac.kr>
2025-04-30 23:13:47 +0900

    2025-04-30

--- Cju bus reservation system/src/Main.java
+++ Cju bus reservation system/src/Main.java
@@ -3,9 +3,41 @@ package  reservation;
 import java.util.Random;
 import java.util.Scanner;
 
-public class Main {
+public class BUS ReservationSystem {
+    static int Total Seat = 20;
+    static Scanner sc = new Scanner(System.in);
+    static Random rand = new Random();
 
-    public static void main(String[] args) {
+    static class seat {
+        static int Seat;
+        String reservedBy;
+
+        public seat(int seat, String reservedBy) {
+            this reservedBy = null;
+            Seat = seat;
+        }
+
+        @Override
+        public boolean isReseved() {
+            return reservedBy != null;
+        }
+
+        public void reserve(String name) {
+            this.reservedBy = name;
+        }
+
+        public String getReservedBy() {
+            return reservedBy;
+        }
+    }
+        @Override
+        public String toString() {
+             return "Seat: " + Seat;
+        }
+
+        public static void main(String[] name) {
+        this reserved By ! = null;
+    }
         Reserve seat = new Reserve();
         seat.run();
     }
